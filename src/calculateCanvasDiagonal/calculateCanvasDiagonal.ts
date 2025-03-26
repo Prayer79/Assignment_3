cdc5747f0547724e6295522fb379df5bd8e67884
Prayerson <prayernoveno@gmail.com>
2025-03-26 15:12:01 -0400

    fix: corrected canvas diagonal calculation and fixed main.ts

--- src/calculateCanvasDiagonal/calculateCanvasDiagonal.ts
+++ src/calculateCanvasDiagonal/calculateCanvasDiagonal.ts
@@ -1,6 +1,10 @@
-export default function calculateCanvasSize(
-  length: string,
-  width: string
-) {
-  return 2 * (parseInt(length) + parseInt(width));
+export default function calculateCanvasDiagonal(length: string, width: string) {
+  const l = parseFloat(length);
+  const w = parseFloat(width);
+
+  if (isNaN(l) || isNaN(w)) {
+    return NaN;
+  }
+
+  return Math.sqrt(l * l + w * w);
 }
